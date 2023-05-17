2ec4f8199a01222ea4500170b99647a3aa40b8ac
questrator <drumtrack@ya.ru>
2023-05-17 23:35:39 +0300

    refactor

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 
 let clock = document.getElementById("clock");
 let color = document.getElementById("color");
-let message = document.getElementById("message")
+let message = document.getElementById("message");
 
+function time() {
+    const time = new Date();
+    const timeUS = time.toLocaleTimeString("en-US", {hour: "2-digit", minute: "2-digit", second: "2-digit"});
+    const hours = time.getHours();
+    
+    const digits = timeUS.replace(/\D/g, "");
+    const bg = `#${digits}`;
+    const text = `#${(0xffffff - parseInt(`0x${digits}`)).toString(16)}`;
+
+    const messages = [
+        [0, "Уже XX часов, пора ложиться отдыхать zZzZz"],
+        [1, "Уже XX часа ночи, доделывай дела и спать"],
+        [5, "XX утра, спать уже бессмысленно, смирись"],
+        [6, "Уже XX утра, пора вставать"],
+        [12, "Сейчас XX часов, бодрствуй!"]
+    ];
+
+    for (let i = messages.length - 1; i >= 0; i--) {
+        if (hours >= messages[i][0]) {
+            message.textContent = messages[i][1].replace("XX", hours > 12 ? hours - 12 : hours);
+            break;
+        }
+    }
+
+    clock.textContent = timeUS;
+    color.textContent = bg;
+    document.body.style.backgroundColor = bg;
+    document.body.style.color = text;
+}
 
 function invertColor(hex) {
     if (hex.indexOf('#') === 0) {
@@ -31,7 +60,7 @@ function rgb() {
     let m = time.getMinutes();
     let s = time.getSeconds();
     let ampm = h >= 12 ? 'PM' : 'AM';
-    console.log(ampm);
+
     // сделал 12 часовой формат и добавил 0 если число меньше 10
     h = h % 12;
     h = h ? h : 12;
@@ -68,6 +97,6 @@ function rgb() {
     document.body.style.color = invertColor(colorStr);
 }
 
-rgb();
-setInterval(rgb, 1000);
+time();
+setInterval(time, 1000);
 
